a79f442125c16351a411a3822f317def04b36430
Mark Home <dumbguy5689@msn.com>
2012-07-26 00:03:17 -0300

    Start

---
+++ CurrentProjectWidget/CurrentProjectWidget/Controllers/CurrentProjectController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CurrentProjectWidget.Controllers
+{
+    public class CurrentProjectController : Controller
+    {
+        public ActionResult CurrentProject()
+        {
+            return Json(new {value = "Hello from MVC Controller"});
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
